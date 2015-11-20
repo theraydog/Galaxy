fa87840c92b152ec9b68a3b187fc217be8eb65ce
Ray Ortgiesen <ray.ortgiesen@gmail.com>
2015-11-20 15:34:45 -0500

    Setting up basic scene/touch controls

---
+++ Galaxy_Project/Assets/Ship_Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship_Player : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Is something touching?
+		if (Input.touchCount > 0) {
+			//Debug.Log (Input.touchCount);
+			
+			
+			/*foreach(Touch touch in Input.touches) {
+				Debug.Log (touch.position);
+			}*/
+			
+			if (Input.GetTouch (0).position.x > (Screen.width/2)) {
+				Debug.Log ("Right");
+				transform.Translate(Vector3.right );
+			}
+			if (Input.GetTouch (0).position.x <= (Screen.width/2)) {
+				Debug.Log ("Left");
+				transform.Translate(Vector3.left);
+
+			}
+
+		}
+	}
+
+	void FixedUpdate() {
+
+	}
+}
---
+++ Galaxy_Project/Assets/TouchTest.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Is something touching?
+		if (Input.touchCount > 0) {
+			//Debug.Log (Input.touchCount);
+
+
+			/*foreach(Touch touch in Input.touches) {
+				Debug.Log (touch.position);
+			}*/
+
+			if (Input.GetTouch (0).position.x > (Screen.width/2)) {
+				Debug.Log ("Right");
+			}
+			if (Input.GetTouch (0).position.x <= (Screen.width/2)) {
+				Debug.Log ("Left");
+			}
+
+			/*if(Input.GetTouch (0).phase == TouchPhase.Began) {
+				Debug.Log ("Touch Began");
+			}
+
+			if(Input.GetTouch (0).phase == TouchPhase.Moved) {
+				Debug.Log ("Touch Moved");
+			}
+
+			if(Input.GetTouch (0).phase == TouchPhase.Ended) {
+				Debug.Log ("Touch Ended");
+			}*/
+		}
+	
+	}
+}
